4f730e5ae20e0117b962d989292c2ef74b601234
“Peerlator” <peer.rheinboldt@gmail.com>
2018-04-29 16:44:57 +0200

    edited setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(name='EasyRegression',
       version='0.1',
-      description='A Plug n' Play library for poliynomial regression',
+      url='https://github.com/peerlator/easyregression',
+      description="A Plug n' Play library for poliynomial regression",
       author='Peerlator',
       packages=['EasyRegression'],
       zip_safe=False)
