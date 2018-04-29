838e6fbbd505770ece4a9fa3c32d9e272e3761bc
“Peerlator” <peer.rheinboldt@gmail.com>
2018-04-29 16:40:45 +0200

    First commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='EasyRegression',
+      version='0.1',
+      description='A Plug n' Play library for poliynomial regression',
+      author='Peerlator',
+      packages=['EasyRegression'],
+      zip_safe=False)
